11014ed29e3756a82c0b88342ff33a4f2aab2406
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 19:44:20 +0530

    Create BTS shown in the figure

--- Program.cs
+++ Program.cs
@@ -6,16 +6,24 @@ namespace HashBinary
     {
         public static void Main(string[] args)
         {
-        
-                Console.WriteLine(" Hash and Binary Table Program  ");
-                BinaryTree b1 = new BinaryTree();
-                b1.Add(56);
-                b1.Add(30);
-                b1.Add(70);
 
-                b1.TraversePreorder(b1.root);
+            BinaryTree b1 = new BinaryTree();
+            b1.Add(56);
+            b1.Add(30);
+            b1.Add(70);
+            b1.Add(60);
+            b1.Add(95);
+            b1.Add(65);
+            b1.Add(63);
+            b1.Add(67);
+            b1.Add(22);
+            b1.Add(40);
+            b1.Add(11);
+            b1.Add(3);
+            b1.Add(16);
 
+            b1.TraversePreorder(b1.root);
 
-            }
         }
+    }
 }
