c3349dd5734dd080763c2ff7f8529aaf7838714d
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 13:20:42 +0530

    Welcome to Hash Tables BST

--- Program.cs
+++ Program.cs
@@ -9,14 +9,7 @@ namespace HashBinary
             Console.WriteLine(" Hash and Binary Table Program  ");
             Console.WriteLine("Hash table demo");
 
-            MyMapNode<string, string> hash = new MyMapNode<string, string>[5];
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-
+         
         }
     }
 }
