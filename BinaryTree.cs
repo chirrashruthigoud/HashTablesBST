9dedde82d78d3054f1dfc135cc8e78c481102af9
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 19:44:30 +0530

    Create BTS shown in the figure

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -61,6 +61,10 @@ namespace HashTablesBST
                 Console.WriteLine(Parent.data + " ");
                 TraversePreorder(Parent.LeftNode);
                 TraversePreorder(Parent.RightNode);
+                Console.WriteLine(Parent.data + " ");
+                TraversePreorder(Parent.LeftNode);
+                TraversePreorder(Parent.RightNode);
+
             }
         }
     }
